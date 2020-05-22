65f7b6e4ca7b8b793e9c459ac8784c746b471fcb
deathquin <roehd007>
2020-05-22 23:41:10 +0900

    Health Check Add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/health", async function(req, res){
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Content-Length': 2
+  });
+  res.write('OK');
+  res.end();
 });
 
 module.exports = router;
